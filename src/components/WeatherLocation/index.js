f7eb01d00d1fcf29c5f5cc8dacc28966fade087c
gonzalezjonathanjesus <gonzalezjonathanjesus@gmail.com>
2019-02-08 00:02:00 -0300

    Component LifeCycle, Loading indication, Material design use and installation, config and use of ESLinter, Use of Props in class component

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,51 +1,80 @@
 import React, { Component } from 'react'; // Import React class from react package
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { PropTypes } from 'prop-types';
+import getUrlWeatherByCity from './../../services/getUrlWeatherByCity'
+import transformWeather from './../../services/transformWeather';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
-import{ SUN, WINDY } from './../../constants/weathers'; //Only import constants which we will use and not all for lightweight code
-
-const data = {
-    temperature: 31,
-    weatherState: SUN,
-    humidity: 10,
-    wind: `10 m/s`,
-}
-
-const data2 = {
-    temperature: 15,
-    weatherState: WINDY,
-    humidity: 20,
-    wind: `19 m/s`,
-}
+// import{ SUN } from './../../constants/weathers'; //Only import constants which we will use and not all for lightweight code
 
 class WeatherLocation extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        const { city } = props;
         this.state = { // Only can be modify on the constructor, after constructor you must to use this.setState(); 
-            city: 'Buenos Aires',
-            data: data
+            city,
+            data: null
         };
+
+        console.log("constructor");
     }
 
-    handleUpdateClick = () => {
-        console.log('Actualizado');
+    componentDidMount() { // Sirve para llamadas al servidor e inicializar cosas
+        console.log("componentDidMount");
+        this.handleUpdateClick();
+    }
 
-        this.setState({ // No es necesario pasar los datos que no van a ser actualizados
-            data: data2
+    componentDidUpdate(prevProps, prevState) {
+        console.log("componentDidUpdate");
+    }
+    
+    /*
+    componentWillMount() {
+        console.log("UNSAFE componentWillMount"); // Están desaconsejadas y van a dejar de existir en la versión 17
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log("UNSAFE componentWillUpdate"); // Están desaconsejadas y van a dejar de existir en la versión 17
+    }
+    */
+    
+    
+
+    handleUpdateClick = () => {
+        const api_weather = getUrlWeatherByCity(this.state.city);
+
+        fetch(api_weather).then( resolve => {
+            return resolve.json()
+        }).then( data => {
+            console.log("Resultado del handleUpdateClick")
+            const newWeather = transformWeather(data);
+            console.log(newWeather);
+            //debugger;
+            this.setState({
+                data: newWeather
+            });
         });
     }
 
     render() {
+        console.log("render");
         const {city, data} = this.state;
         return (
             <div className="weatherLocationCont">
                 <Location city={city}></Location>
-                <WeatherData data={data}></WeatherData>
-                <button onClick={this.handleUpdateClick}>Actualizar</button>
+                {data ? // Operador ternario if
+                    <WeatherData data={data}></WeatherData> :
+                    <CircularProgress size={50}/>
+                }
             </div> // Babel JSX template that will be rendered
         );
     }
 };
+
+WeatherLocation.propTypes = {
+    city: PropTypes.string.isRequired,
+}
 
 export default WeatherLocation; // The rest of the app can be invoke it because we use default
---
+++ src/constants/api_url.js
@@ -0,0 +1,3 @@
+
+export const api_key = "5ed8339b2e7a3ad43f526122af4871a7";
+export const url_base_weather = "https://api.openweathermap.org/data/2.5/weather";
